894edaa52f60027d5b0529f19cebe8f137367b3b
POP CODE <lolo4ka69@list.ru>
2023-05-25 15:44:05 +0300

    Commit add test

--- src/test/java/ru/netology/FilmManagerTest.java
+++ src/test/java/ru/netology/FilmManagerTest.java
@@ -23,19 +23,34 @@ public class FilmManagerTest {
     }
 
     @Test
-    public void lastFilms() {
+    public void EqualsLimit() {
         String film1 = "Movie 1";
         String film2 = "Movie 2";
         String film3 = "Movie 3";
+        String film4 = "Movie 4";
+        String film5 = "Movie 5";
+        String film6 = "Movie 6";
+        String film7 = "Movie 7";
+        String film8 = "Movie 8";
+        String film9 = "Movie 9";
+        String film10 = "Movie 10";
 
 
-        FilmManager manager = new FilmManager();
-        manager.addFilm(film3);
-        manager.addFilm(film2);
+        FilmManager manager = new FilmManager(10);
+
         manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
 
-        String[] actual = manager.findAll();
-        String[] expected = {film3, film2, film1};
+        String[] actual = manager.findLast();
+        String[] expected = {film10, film9, film8, film7, film6, film5, film4, film3, film2, film1};
 
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -71,6 +86,33 @@ public class FilmManagerTest {
 
         String[] actual = manager.findLast();
         String[] expected = {film10, film9, film8, film7, film6,};
+
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void GetLastUnderLimit() {
+        String film1 = "Movie 1";
+        String film2 = "Movie 2";
+        String film3 = "Movie 3";
+        String film4 = "Movie 4";
+        String film5 = "Movie 5";
+
+
+        FilmManager manager = new FilmManager(10);
+
+
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+
+
+        String[] actual = manager.findLast();
+        String[] expected = {film5, film4, film3, film2, film1};
 
 
         Assertions.assertArrayEquals(expected, actual);
